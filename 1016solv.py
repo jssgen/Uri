538110c9b5ec9419565a6a22e60d8f551d3850bf
drope <alvopedro0@hotmail.com>
2021-03-13 02:50:59 -0300

    1016solved.py
    uri 1016 solved

---
+++ 1016solv.py
@@ -0,0 +1,14 @@
+#carro 1 = 60km/h
+#carro 2 = 90km/h
+#diferença de distancia = 30km
+#1km a cada 2 min.
+
+#ler a diferença de distancia entre carro 1 e 2
+
+dif = int(input())
+
+#calcular o tempo
+
+tempo = dif * 2
+
+print(tempo, "minutos")
