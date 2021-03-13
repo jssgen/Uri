9ed0d30cd3e56b766279e964895a0fa15a85f4b0
drope <alvopedro0@hotmail.com>
2021-03-13 02:58:06 -0300

    1035solved.py
    uri 1035 solved

---
+++ 1035solv.py
@@ -0,0 +1,14 @@
+#leia 4 valores inteiros: A,B,C,D
+
+valores = input().split()
+a, b, c, d = valores
+
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+
+if b > c and d > a and (c + d) > (a + b) and c > 0 and d > 0 and a % 2 == 0:
+    print("Valores aceitos")
+else:
+    print("Valores nao aceitos")
